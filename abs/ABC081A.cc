94ae57b925474359082abf8d6bb5986e9a897269
false <false@wizard-limit.net>
2019-06-11 09:39:40 +0900

    Placing Marbles

---
+++ abs/ABC081A.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+	if (s[i] == '1') {
+	    count++;
+	}
+    }
+    cout << count << endl;
+    return 0;
+}
