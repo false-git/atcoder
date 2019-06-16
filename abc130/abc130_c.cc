91ee16261cfc4ee283d070925eeef9d16c6b9c7b
false <false@wizard-limit.net>
2019-06-16 22:53:15 +0900

    考えすぎていたらしい
    条件に合う1点が与えられた場合、常に長方形を2等分する直線が引けるので、
    面積は常に半分になる。
    複数解があるのは、(x, y)が中心のとき。

--- abc130/abc130_c.cc
+++ abc130/abc130_c.cc
@@ -7,27 +7,10 @@ int main()
     unsigned long long W, H, x, y;
     cin >> W >> H >> x >> y;
     int multi = 0;
-    // x, y を常に原点に近い側に移動。
-    if (x * 2 > W) {
-	x = W - x;
-    }
-    if (y * 2 > H) {
-	y = H - y;
-    }
-    double sx = x * H;
-    double sy = y * W;
-    double s;
-    if (x == 0 && y == 0) {
-	// x, y が原点になるとき。
-	s = W / 2.0 * H;
-    } else if (sx == sy) {
+    double s = W * H / 2.0;
+    if (x * 2 == W && y * 2 == H) {
 	// 最適な分割が複数ある。
-	s = sx;
 	multi = 1;
-    } else if (sx < sy) {
-	s = sy;
-    } else {
-	s = sx;
     }
     cout << s << " " << multi << endl;
     return 0;
