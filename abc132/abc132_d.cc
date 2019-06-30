289d11ca4d1b42604bbab94211a0ec43a907130c
false <false@wizard-limit.net>
2019-06-30 16:15:55 +0900

    再帰であがいたもの
    2000 3の時点で、死ぬほど時間がかかる。
    解説を読むと、順列組み合わせの公式を使って解くらしい。

--- abc132/abc132_d.cc
+++ abc132/abc132_d.cc
@@ -2,9 +2,65 @@
 
 using namespace std;
 
+static constexpr int MOD = 1000000000 + 7;
+
+/**
+ * 操作回数を取得する
+ * @param L ボールの並べ方
+ * @param N ボールの数
+ * @return 操作回数
+ */
+int check(int L[], int N) {
+    int count = 0;
+    bool blue = false;
+    for (int i = 0; i < N; i++) {
+	if (L[i] == 0) {
+	    if (!blue) {
+		blue = true;
+		count++;
+	    }
+	} else {
+	    if (blue) {
+		blue = false;
+	    }
+	}
+    }
+    return count;
+}
+
+/**
+ * pの位置にある赤いボールを左に動かす
+ * @param C 操作回数
+ * @param L ボールの並べ方
+ * @param N ボールの数
+ * @param p 動かす赤いボールの位置
+ * @param left 動かせる左限
+ */
+int move_ball(int C[], int L[], int N, int p, int limit) {
+    for (int i = p; i > limit; i--) {
+	L[i] = 0;
+	L[i - 1] = 1;
+	// 操作回数(青が分断された数)を計算する。
+	int count = check(L, N);
+	C[count - 1] = (C[count - 1] + 1) % MOD;
+#if 0
+	for (int i = 0; i < N; i++) {
+	    cout << L[i] << " ";
+	}
+	cout << count << endl;
+#endif
+	if (p < N - 1) {
+	    move_ball(C, L, N, p + 1, i);
+	}
+    }
+    // 元に戻す
+    L[p] = 1;
+    L[limit] = 0;
+    return 0;
+}
+
 int main()
 {
-    constexpr int MOD = 1000000000 + 7;
     int N, K;
     cin >> N >> K;
     int L[N]; // ボールの並べ方(index: 位置)
@@ -17,48 +73,7 @@ int main()
 	L[i] = 1; // 1は赤いボール
     }
     C[0] = 1; // 初期状態は1回の操作で取れる。
-    int pivot = K; // 交換位置(最初に青の次に赤があるところ)
-    while (true) {
-	// pivotとその前を入れ換える
-	L[pivot - 1] = 1;
-	L[pivot] = 0;
-
-	// 操作回数(青が分断された数)を計算する。
-	int count = 0;
-	bool blue = false;
-	for (int i = 0; i < N; i++) {
-	    if (L[i] == 0) {
-		if (!blue) {
-		    blue = true;
-		    count++;
-		}
-	    } else {
-		if (blue) {
-		    blue = false;
-		}
-	    }
-	}
-	C[count - 1] = (C[count - 1] + 1) % MOD;
-	for (int i = 0; i < N; i++) {
-	    cout << L[i] << " ";
-	}
-	cout << count << endl;
-
-	// 新しいpivotを決める。
-	if (pivot > 1 && L[pivot - 2] == 0) {
-	    pivot--;
-	} else {
-	    int prevpivot = pivot;
-	    for (int i = pivot + 1; i < N; i++) {
-		if (L[i] == 1) {
-		    pivot = i;
-		}
-	    }
-	    if (prevpivot == pivot) {
-		break;
-	    }
-	}
-    }
+    move_ball(C, L, N, K, 0);
     for (int i = 0; i < K; i++) {
 	cout << C[i] << endl;
     }
